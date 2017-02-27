43e71d0f0c0030acb219806d5673fae43c31f7b9
Lewis Haggar <lhaggar@outlook.com>
2017-02-27 10:30:11 +0000

    Fixed _build serve path

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const path = require('path');
 const api = require('./api');
 
 const PORT = 3000;
-const BUILD_FOLDER = path.resolve('../_build');
+const BUILD_FOLDER = path.resolve(__dirname, '../_build');
 
 const app = express();
 
 app.use('/api', api.create());
-app.use(express.static(__dirname + BUILD_FOLDER));
+app.use(express.static(BUILD_FOLDER));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
